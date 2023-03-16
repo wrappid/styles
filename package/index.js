5d186847ed45c91f419d30905cb4e3740e7f8898
techoneel <contact.techoneel@gmail.com>
2023-03-16 22:03:06 +0530

    + index file added

---
+++ package/index.js
@@ -0,0 +1,40 @@
+import CoreClasses from "./CoreClasses";
+import { getEffectiveStyle } from "./StyleUtil";
+import UtilityClasses from "./UtilityClasses";
+import StyledComponentsClasses from "./StyledComponentsClasses";
+import {
+  ENV_DEV_MODE,
+  ENV_STAGE_MODE,
+  ENV_PROD_MODE,
+  SMALL_WINDOW_WIDTH,
+  MEDIUM_WINDOW_WIDTH,
+  LARGE_WINDOW_WIDTH,
+  X_LARGE_WINDOW_WIDTH,
+  XX_LARGE_WINDOW_WIDTH,
+} from "./config/constants";
+export {
+  /**
+   * Style Classes Constants
+   */
+  CoreClasses,
+  UtilityClasses,
+  StyledComponentsClasses,
+  /**
+   * Utility Function(s)
+   */
+  getEffectiveStyle,
+  /**
+   * Constants
+   */
+  ENV_DEV_MODE,
+  ENV_STAGE_MODE,
+  ENV_PROD_MODE,
+  /**
+   * Window Width Constants
+   */
+  SMALL_WINDOW_WIDTH,
+  MEDIUM_WINDOW_WIDTH,
+  LARGE_WINDOW_WIDTH,
+  X_LARGE_WINDOW_WIDTH,
+  XX_LARGE_WINDOW_WIDTH,
+};
