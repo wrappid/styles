ef8d433deee64709794d05af0b1fca430b29e3de
sumanta-m <sumanta.m@vixplor.com>
2023-06-26 18:25:44 +0530

    configartion overrider exported

--- package/index.js
+++ package/index.js
@@ -2,21 +2,25 @@ import { getEffectiveStyle } from "./StyleUtil";
 import UtilityClasses from "./utility/UtilityClasses";
 import StyledComponentsClasses from "./styledComponents/StyledComponentsClasses";
 import {
-  SMALL_WINDOW_WIDTH,
-  MEDIUM_WINDOW_WIDTH,
-  LARGE_WINDOW_WIDTH,
-  X_LARGE_WINDOW_WIDTH,
-  XX_LARGE_WINDOW_WIDTH
+	SMALL_WINDOW_WIDTH,
+	MEDIUM_WINDOW_WIDTH,
+	LARGE_WINDOW_WIDTH,
+	X_LARGE_WINDOW_WIDTH,
+	XX_LARGE_WINDOW_WIDTH
 } from "./config/constants";
 import { overrideThemeConfiguration } from "./theme/theme";
+import { getConfigurationObject } from "./helper/helper";
 import StylesProvider from "./StylesProvider";
-import { IMPORTANT, DEFAULT_PADDING, defaultUtilityStyles } from "./utility/DefaultUtilityStyles";
+import {
+	IMPORTANT,
+	DEFAULT_PADDING,
+	defaultUtilityStyles
+} from "./utility/DefaultUtilityStyles";
 import { smallUtilityStyles } from "./utility/SmallUtilityStyles";
 import { mediumUtilityStyles } from "./utility/MediumUtilityStyles";
 import { largeUtilityStyles } from "./utility/LargeUtilityStyles";
 import { xLargeUtilityStyles } from "./utility/XLargeUtilityStyles";
 import { xXLargeUtilityStyles } from "./utility/XXLargeUtilityStyles";
-
 
 export {
 	/**
@@ -29,25 +33,25 @@ export {
 	largeUtilityStyles,
 	xLargeUtilityStyles,
 	xXLargeUtilityStyles,
-
-  /**
-   * Style Classes Constants
-   */
-  StyledComponentsClasses,
-  /**
-   * Utility Function(s)
-   */
-  getEffectiveStyle,
-  /**
-   * Window Width Constants
-   */
-  SMALL_WINDOW_WIDTH,
-  MEDIUM_WINDOW_WIDTH,
-  LARGE_WINDOW_WIDTH,
-  X_LARGE_WINDOW_WIDTH,
-  XX_LARGE_WINDOW_WIDTH,
-  overrideThemeConfiguration,
-  StylesProvider,
-  DEFAULT_PADDING,
+	/**
+	 * Style Classes Constants
+	 */
+	StyledComponentsClasses,
+	/**
+	 * Utility Function(s)
+	 */
+	getEffectiveStyle,
+	/**
+	 * Window Width Constants
+	 */
+	SMALL_WINDOW_WIDTH,
+	MEDIUM_WINDOW_WIDTH,
+	LARGE_WINDOW_WIDTH,
+	X_LARGE_WINDOW_WIDTH,
+	XX_LARGE_WINDOW_WIDTH,
+	overrideThemeConfiguration,
+	StylesProvider,
+	getConfigurationObject,
+	DEFAULT_PADDING,
 	IMPORTANT
 };
