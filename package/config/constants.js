ae75696c24316c46eb86051bb0e5eec1843a1dd9
Sam <sam@rxefy.com>
2023-09-09 18:18:04 +0530

    add size specific padding and margin classes

--- package/config/constants.js
+++ package/config/constants.js
@@ -4,12 +4,13 @@
 // export const LARGE_WINDOW_WIDTH = "992";
 // export const X_LARGE_WINDOW_WIDTH = "1200";
 // export const XX_LARGE_WINDOW_WIDTH = "1400";
+
 // mui - BREAKPOINTS
 export const SMALL_WINDOW_WIDTH = "600";
 export const MEDIUM_WINDOW_WIDTH = "900";
 export const LARGE_WINDOW_WIDTH = "1200";
 export const X_LARGE_WINDOW_WIDTH = "1536";
-// applying same width of XLARGE
-// since it's not present in MUI-Breakpoints
+// since XX_LARGE is not present in MUI-Breakpoints
+// applying same width as X_LARGE
 // so it could be supported later on
 export const XX_LARGE_WINDOW_WIDTH = "1536";
